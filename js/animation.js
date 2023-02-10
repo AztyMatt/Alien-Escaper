7d7139cfffa8c3e073b811d6d3f46ca392ed3922
AztyUwU <benderpro102@gmail.com>
2023-02-10 10:08:33 -0500

    diapo avec animation

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', () => {
 
-    if (document.querySelector('main') !== null){
+
+    //Accueil
+    if (document.querySelector('#accueil') !== null){
         const TL = gsap.timeline({paused: true});
-        console.log("detect")
     
     TL
     .from("#background", {autoAlpha:0, duration:1, ease:"linear"})
@@ -10,5 +11,24 @@ window.addEventListener('load', () => {
     .from("#commands button", {autoAlpha:0, duration:0.5, ease:"linear"})
 
         TL.play();
+    }
+
+
+    //Intro
+    if (document.querySelector('#intro') !== null){
+        const TL = gsap.timeline({paused: true});
+
+        var boxes = $(".image");
+        let i=0
+        TweenMax.to(boxes[i], 3, {autoAlpha:1, ease:"ease-out"});
+
+        $("#right").click(function(){
+            i += 1
+            TweenMax.to(boxes[i], 3, {autoAlpha:1, ease:"ease-out"});
+        })
+
+        $("#left").click(function(){
+            i -= 1
+        })
     }
 });
